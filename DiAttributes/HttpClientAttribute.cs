9619a83eb9eaa73ed63f59a6739a3fe28eefc9b7
Toby Smith <tobysmith568@hotmail.co.uk>
2022-01-21 18:26:00 +0000

    Add an http client attribute

---
+++ DiAttributes/HttpClientAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DiAttributes
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class HttpClientAttribute : Attribute
+    {
+        /// <param name="serviceType">The service type to register the class against; usually an interface</param>
+        public HttpClientAttribute(Type serviceType)
+        {
+            ServiceType = serviceType;
+        }
+
+        public HttpClientAttribute()
+        {
+        }
+
+        public Type? ServiceType { get; }
+    }
+}
--- DiAttributes/RegisterExtensions.cs
+++ DiAttributes/RegisterExtensions.cs
@@ -12,10 +12,13 @@ namespace DiAttributes
     {
         private const string NullConfigurationException = "A class was decorated with the Configuration attribute " +
             "but RegisterDiAttributes was called without passing in an IConfiguration.";
+
         private static MethodInfo? configureMethod;
+        private static MethodInfo? addHttpClientMethodForType;
+        private static MethodInfo? addHttpClientMethodForTypeAndInterface;
 
         /// <summary>
-        /// Registers all classes that are decorated with the Scoped, Transient, and Singleton attributes.
+        /// Registers all classes that are decorated with the Scoped, Transient, Singleton, and HttpClient attributes.
         /// 
         /// To use the Configuration attribute you need to use the overload which accepts an IConfiguration.
         /// </summary>
@@ -25,7 +28,7 @@ namespace DiAttributes
         }
 
         /// <summary>
-        /// Registers all classes that are decorated with the Scoped, Transient, Singleton, and Configuration attributes.
+        /// Registers all classes that are decorated with the Scoped, Transient, Singleton, HttpClient, and Configuration attributes.
         /// 
         /// If you're not using the Configuration attribute then you can use the overload which doesn't need an IConfiguration.
         /// </summary>
@@ -72,6 +75,12 @@ namespace DiAttributes
                 if (customAttribute.AttributeType == typeof(TransientAttribute))
                 {
                     RegisterTransientAttribute(services, @class, customAttribute);
+                    return;
+                }
+
+                if (customAttribute.AttributeType == typeof(HttpClientAttribute))
+                {
+                    RegisterHttpClientAttribute(services, @class, customAttribute);
                     return;
                 }
 
@@ -141,6 +150,34 @@ namespace DiAttributes
             }
         }
 
+        private static void RegisterHttpClientAttribute(IServiceCollection services, Type injectable, CustomAttributeData customAttributeData)
+        {
+            if (customAttributeData.AttributeType != typeof(HttpClientAttribute))
+            {
+                return;
+            }
+
+            MethodInfo addHttpClientMethod;
+            if (customAttributeData.ConstructorArguments.Count > 0)
+            {
+                var serviceType = (Type)customAttributeData.ConstructorArguments[0].Value;
+                addHttpClientMethod = GetAddHttpClientMethodForGenerics(serviceType, injectable);
+            }
+            else
+            {
+                addHttpClientMethod = GetAddHttpClientMethodForGenerics(injectable);
+            }
+
+            try
+            {
+                addHttpClientMethod.Invoke(services, new object[] { services });
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException($"Unabled to register the class {injectable.FullName} as an HttpClient", ex);
+            }
+        }
+
         private static void RegisterConfigurationAttribute(IServiceCollection services, IConfiguration config, Type injectable, CustomAttributeData customAttributeData)
         {
             if (customAttributeData.AttributeType != typeof(ConfigurationAttribute))
@@ -166,33 +203,65 @@ namespace DiAttributes
             }
         }
 
+        private static MethodInfo GetAddHttpClientMethodForGenerics(params Type[] genericTypes)
+        {
+            if (addHttpClientMethodForTypeAndInterface != null)
+            {
+                return addHttpClientMethodForTypeAndInterface.MakeGenericMethod(genericTypes);
+            }
+
+            var extensionMethods = GetAllAddHttpClientExtensionMethods();
+
+            var extMethodsWithCorrectSignature = extensionMethods.Where(method =>
+            {
+                const string ConfigureMethodName = "AddHttpClient";
+                if (method.Name != ConfigureMethodName)
+                    return false;
+
+                var parameters = method.GetParameters();
+
+                if (parameters.Length != 1)
+                    return false;
+
+                if (parameters[0].ParameterType != typeof(IServiceCollection))
+                    return false;
+
+                var genericArguments = method.GetGenericArguments();
+
+                if (genericArguments.Length != 2)
+                    return false;
+
+                return true;
+            });
+
+            if (extMethodsWithCorrectSignature.Count() != 1)
+            {
+                const string ErrorMessage = "Found more than one IServiceCollection.AddHttpClient extension method";
+                throw new InvalidOperationException(ErrorMessage);
+            }
+
+            var correctExtensionMethod = extMethodsWithCorrectSignature.SingleOrDefault();
+
+            if (correctExtensionMethod == null)
+            {
+                const string ErrorMessage = "Unable to find the IServiceCollection.AddHttpClient extension method";
+                throw new InvalidOperationException(ErrorMessage);
+            }
+
+            addHttpClientMethodForTypeAndInterface = correctExtensionMethod;
+            return addHttpClientMethodForTypeAndInterface.MakeGenericMethod(genericTypes);
+        }
+
         private static MethodInfo GetConfigureMethodForGenericType(Type genericType)
         {
-            const BindingFlags StaticMethodBindingFlags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
-
             if (configureMethod != null)
             {
                 return configureMethod.MakeGenericMethod(new Type[] { genericType });
             }
 
-            var allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var extensionMethods = GetAllConfigurationExtensionMethods();
 
-            var configurationAssemblies = allAssemblies.Where(a =>
-            {
-                const string ConfigurationAssemblyName = "Microsoft.Extensions.Options.ConfigurationExtensions,";
-                return a.FullName.StartsWith(ConfigurationAssemblyName);
-            });
-
-            var allTypes = configurationAssemblies.SelectMany(a => a.GetTypes());
-
-            var sealedNonGenericTypes = allTypes
-                .Where(t => t.IsSealed && !t.IsGenericType && !t.IsNested);
-
-            var extensionMethods = sealedNonGenericTypes
-                .SelectMany(t => t.GetMethods(StaticMethodBindingFlags))
-                .Where(method => method.IsDefined(typeof(ExtensionAttribute), false));
-
-            var extMethodsWithCorrectSignature = extensionMethods.Where<MethodInfo>(method =>
+            var extMethodsWithCorrectSignature = extensionMethods.Where(method =>
             {
                 const string ConfigureMethodName = "Configure";
                 if (method.Name != ConfigureMethodName)
@@ -228,6 +297,40 @@ namespace DiAttributes
 
             configureMethod = correctExtensionMethod;
             return correctExtensionMethod.MakeGenericMethod(new Type[] { genericType });
+        }
+
+        private static IEnumerable<MethodInfo> GetAllConfigurationExtensionMethods()
+        {
+            return GetExtensionMethodsInAssembly("Microsoft.Extensions.Options.ConfigurationExtensions");
+        }
+
+        private static IEnumerable<MethodInfo> GetAllAddHttpClientExtensionMethods()
+        {
+            return GetExtensionMethodsInAssembly("Microsoft.Extensions.Http");
+        }
+
+        private static IEnumerable<MethodInfo> GetExtensionMethodsInAssembly(string assemblyName)
+        {
+            const BindingFlags StaticMethodBindingFlags = BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+
+            var allAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            var configurationAssemblies = allAssemblies.Where(a =>
+            {
+                var assemblyNameWithComma = assemblyName + ',';
+                return a.FullName.StartsWith(assemblyNameWithComma);
+            });
+
+            var allTypes = configurationAssemblies.SelectMany(a => a.GetTypes());
+
+            var sealedNonGenericTypes = allTypes
+                .Where(t => t.IsSealed && !t.IsGenericType && !t.IsNested);
+
+            var extensionMethods = sealedNonGenericTypes
+                .SelectMany(t => t.GetMethods(StaticMethodBindingFlags))
+                .Where(method => method.IsDefined(typeof(ExtensionAttribute), false));
+
+            return extensionMethods;
         }
     }
 }
